40fa052b4f4f49e2efb888c9b26ba78e04d8bac7
donnlouco <vinicius.lima6@estudante.ifms.edu.br>
2025-11-30 20:27:30 -0300

    grandes avancos

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ window.addEventListener("scroll", () => {
         header.classList.remove("scrolled");
     }
 })
+
+const slidesSobre = document.querySelectorAll('.slide-item');
+let indexSobre = 0;
+
+function rodarSliderSobre() {
+    // Remove a classe active da atual
+    slidesSobre[indexSobre].classList.remove('ativo');
+
+    // Vai para a próxima
+    indexSobre++;
+    
+    // Se acabou, volta para a primeira
+    // verifica se o index vai ficar maior q o numero de fotos
+    // se ficar maior ou igual, ele zera e volta do comeco
+    if (indexSobre >= slidesSobre.length) {
+        indexSobre = 0;
+    }
+
+    // Adiciona a classe active na nova
+    slidesSobre[indexSobre].classList.add('ativo');
+}
+
+// Roda a cada 4 segundos
+setInterval(rodarSliderSobre, 4000);
