181e805f5db0d62b6992accf5d358f79bf6c5cb0
donnlouco <vinicius.lima6@estudante.ifms.edu.br>
2025-12-07 22:34:01 -0300

    fe

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", () => {
     const header = document.querySelector(".header");
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 30) {
         header.classList.add("scrolled");
     }
     else{
